f9c985cb191c05741fb7db2b736db365b156e442
Anastasios Grammenos <avgramme@ece.auth.gr>
2017-12-02 19:53:28 +0200

    finall

--- Adventure/main.cpp
+++ Adventure/main.cpp
@@ -76,110 +76,6 @@ public:
 	}
 };
 
-void game() {
-	cout << "Welcome to the *cough* only *cough* open-source Higher-Lower game! We use..." << endl;
-	std::cin.get();
-
-	// LOADING QUESTIONS FROM FILE
-	cout << "Loading Questions..." << endl;	
-	fstream QnAfile;
-	QnAfile.open("Q&A.txt");
-	string line, sent;
-	int saidAns, correctAnsw;
-
-	vector<Sentence> mySentences;
-	// GENERAL GAME VARIABLES
-	int lives = 3;
-	int points = 0;
-	int new_points = 0;
-	vector<int> crossedOut;
-	bool flag = false;
-	
-	while (getline(QnAfile, sent))
-	{
-		getline(QnAfile, line);
-		saidAns = stoi(line);
-		getline(QnAfile, line);
-		correctAnsw = stoi(line);
-		//cout << sent << "\t" << saidAns << "\t" << correctAnsw << endl;
-		mySentences.push_back(Sentence(sent, saidAns, correctAnsw));
-	}
-
-	// FINISHED LOADING
-	
-	
-	
-	while (lives > 0) {
-		int select;
-		
-		do {
-			flag = false;
-			srand(time(NULL));
-			select = rand() % mySentences.size();
-			for (unsigned int i = 0; i < crossedOut.size(); i++) {
-				if (select == crossedOut[i])
-					flag = true;
-			}
-		} while (flag == true);
-
-		crossedOut.push_back(select);
-		cout << "Question:" << endl;
-		cout << mySentences[select].getSentence() << endl;
-		char myAnswer[256];
-		int translation;
-		std::cin >> myAnswer;
-		if (myAnswer[0] == 'h' || myAnswer[0] == 'H')
-			translation = 1;
-		else if (myAnswer[0] == 'l' || myAnswer[0] == 'L')
-			translation = 0;
-		else {
-			cout << "You need to write a proper answer! You lose one life >:( " << endl;
-			translation = 2;
-		}
-
-		if (translation == mySentences[select].getRes()) {
-			new_points = (int)(mySentences[select].getCorrAnsw() - mySentences[select].getSaidAnsw())*(mySentences[select].getCorrAnsw() - mySentences[select].getSaidAnsw());
-			points += new_points;
-			cout << endl << "Spot on!" << endl;
-			cout << "The correct answer was " << mySentences[select].getCorrAnsw() << endl;
-			cout << "... which nets you " << new_points << " points for a total of " << points << " !" << endl;
-		}
-		else {
-			cout << endl << "That was embarassing..." << endl;
-			cout << "Your lives get reduced to " << --lives << " !" << endl;
-		}
-		
-		system("pause");
-		if (crossedOut.size() == mySentences.size()) {
-			cout << "We're all out of questions! Good job for getting this far..." << endl;
-			lives = 0;
-		}
-		
-		
-		/*cout << mySentences[select].getSentence() << endl;
-		int myAnswer;
-		cin >> myAnswer;
-		if (myAnswer == mySentences[select].getRes()) {
-			new_points = (int)(mySentences[select].getCorrAnsw() - mySentences[select].getSaidAnsw())*(mySentences[select].getCorrAnsw() - mySentences[select].getSaidAnsw());
-			points += new_points;
-			cout << endl << "Spot on!" << endl;
-			cout << "The correct answer was " << mySentences[select].getCorrAnsw() << endl;
-			cout << "... which nets you " << new_points << " points for a total of " << points << " !" << endl;
-		}
-			
-		else {
-			cout << endl << "That was embarassing..." << endl;
-			cout << "Your lives get reduced to " << --lives << " !" << endl;
-		}*/
-		
-	}
-
-	cout << "Game Over!" << endl << "Your final count is " << points << " points!" << endl;
-	
-	//cin.get();*/
-}
-
-
 int main()
 {
 	cout << "Welcome to the *cough* only *cough* open-source Higher-Lower game! We use GitHub's repository environment along with Visual Studio's integration." << endl;
@@ -255,7 +151,7 @@ int main()
 		cout << "We're all out of questions! Good job for getting this far..." << endl;
 	cout << "Game Over!" << endl << "Your final count is " << points << " points!" << endl << endl;
 	
-	//game();
+
 	cout << "Press any key to exit." << endl;
 	std::cin.clear();
 	std::cin.ignore(256, '\n');
